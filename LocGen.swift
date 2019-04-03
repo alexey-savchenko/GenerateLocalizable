acb5317dbe5bd6f0c4de600100335ed6ac202d19
Alexey Savchenko <nahalem@ukr.net>
2019-04-03 19:14:57 +0300

    Added handling of input tokens

--- LocGen.swift
+++ LocGen.swift
@@ -8,6 +8,11 @@ extension Collection {
   }
 }
 
+enum Token: String {
+  case inputFilePath = "-i"
+  case outputProjectPath = "-o"
+}
+
 func termsDefinitionsToString(_ inputArray: [(String, String)]) -> String {
   var stringBuffer = ""
   
@@ -18,71 +23,75 @@ func termsDefinitionsToString(_ inputArray: [(String, String)]) -> String {
   return stringBuffer
 }
 
-func writeToDisk(language: String, termsAndDefinitions: [(String, String)]) {
-  let targetDirURL = FileManager.default
-    .urls(for: .desktopDirectory, in: .userDomainMask)
-    .first!
-    .appendingPathComponent(language)
-  
-  let resultURL = targetDirURL
-    .appendingPathComponent("Localizable")
-    .appendingPathExtension("strings")
-  
+func write(language: String, termsAndDefinitions: [(String, String)], baseDirURL: URL?) {
+  let targetDirURL = (baseDirURL ?? FileManager.default.urls(for: .documentDirectory,
+                                                             in: .userDomainMask).first!).appendingPathComponent(language + ".lproj")
+  let resultURL = targetDirURL.appendingPathComponent("Localizable").appendingPathExtension("strings")
   let processedString = termsDefinitionsToString(termsAndDefinitions)
-  
-  try? FileManager.default.createDirectory(at: targetDirURL,
-                                           withIntermediateDirectories: true,
-                                           attributes: nil)
-  
-  try! processedString
-    .data(using: String.Encoding.utf8)!
-    .write(to: resultURL)
+  try? FileManager.default.createDirectory(at: targetDirURL, withIntermediateDirectories: true, attributes: nil)
+  try! processedString.data(using: .utf8)!.write(to: resultURL)
 }
 
-if let filePath = CommandLine.arguments[safe: 1] {
-  let localizationFileURL = URL(fileURLWithPath: filePath)
-  guard FileManager.default.fileExists(atPath: localizationFileURL.path) else { fatalError() }
-  
-  let csvContents = try? String(contentsOf: localizationFileURL, encoding: .utf8)
-    .components(separatedBy: "\n")
-    .map { row in row
-      .components(separatedBy: ",")
-      .map { column in column.trimmingCharacters(in: .whitespacesAndNewlines) } }
-  
-  var container: [String: [(String, String)]] = [:]
-  
-  // process csvContents to fill container with structure [Language: [Term: Definition]]
-  
-  var columns: [[String]] = []
-  
-  for (rowIndex, _) in csvContents!.enumerated() {
-    var col = [String]()
-    for (_row) in csvContents! {
-      if let item = _row[safe: rowIndex] {
-        col.append(item)
+func main() {
+  if CommandLine.arguments.count >= 3 {
+    if let inputTokenIndex = CommandLine.arguments.firstIndex(of: Token.inputFilePath.rawValue),
+      let filePath = CommandLine.arguments[safe: inputTokenIndex + 1] {
+      let localizationFileURL = URL(fileURLWithPath: filePath)
+      guard FileManager.default.fileExists(atPath: localizationFileURL.path) else {
+        print("Input file does not exist ⛔️")
+        return
       }
+      
+      let csvContents = try? String(contentsOf: localizationFileURL, encoding: .utf8)
+        .components(separatedBy: "\n")
+        .map { row in row
+          .components(separatedBy: ",")
+          .map { column in column.trimmingCharacters(in: .whitespacesAndNewlines) } }
+      
+      var container: [String: [(String, String)]] = [:]
+      
+      // process csvContents to fill container with structure [Language: [Term: Definition]]
+      
+      var columns: [[String]] = []
+      
+      for (rowIndex, _) in csvContents!.enumerated() {
+        var col = [String]()
+        for (_row) in csvContents! {
+          if let item = _row[safe: rowIndex] {
+            col.append(item)
+          }
+        }
+        if !col.isEmpty {
+          columns.append(col)
+        }
+      }
+      
+      for col in columns[1...] {
+        let lang = col[0].lowercased()
+        var terms = [(String, String)]()
+        
+        for translation in col[1...] {
+          let idx = col.firstIndex(of: translation)!
+          let term = columns[0][idx]
+          terms.append((term, translation))
+        }
+        container[lang] = terms
+      }
+      let outputDirPath = CommandLine.arguments
+        .firstIndex(of: Token.outputProjectPath.rawValue)
+        .flatMap { CommandLine.arguments[safe: $0 + 1] }
+        .map { URL(fileURLWithPath: $0, isDirectory: true) }
+      
+      container.forEach { (arg) in
+        let (lang, terms) = arg
+        write(language: lang, termsAndDefinitions: terms, baseDirURL: outputDirPath)
+      }
+    } else {
+      print("Pass valid file url ⛔️")
     }
-    if !col.isEmpty {
-      columns.append(col)
-    }
+  } else {
+    print("Invalid number arguments passed ⛔️")
   }
-  
-  for col in columns[1...] {
-    let lang = col[0].lowercased()
-    var terms = [(String, String)]()
-    
-    for translation in col[1...] {
-      let idx = col.firstIndex(of: translation)!
-      let term = columns[0][idx]
-      terms.append((term, translation))
-    }
-    container[lang] = terms
-  }
-  
-  container.forEach { (arg) in
-    let (lang, terms) = arg
-    writeToDisk(language: lang, termsAndDefinitions: terms)
-  }
-} else {
-  print("Pass valid file url")
 }
+
+main()
